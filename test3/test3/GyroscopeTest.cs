41ba697bf1fe6b32b84888e202871e582000466d
Olga <Olga@DESKTOP-QINEQLL>
2021-11-09 12:32:37 +0500

    Добавьте файлы проекта.

---
+++ test3/test3/GyroscopeTest.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Essentials;
+using System.ComponentModel;
+
+namespace test3
+{
+    public class GyroscopeTest : INotifyPropertyChanged
+    {
+        // Set speed delay for monitoring changes.
+        SensorSpeed speed = SensorSpeed.UI;
+        private double x = 0;
+        public double X
+        {
+            get { return x; }
+            set
+            {
+                if (x != value)
+                { 
+                    x = value;
+                    OnPropertyChanged("X");
+                }
+            }
+        }
+        private double y = 0;
+        public double Y
+        {
+            get { return y; }
+            set
+            {
+                if (y != value)
+                {
+                    y = value;
+                    OnPropertyChanged("Y");
+                }
+            }
+        }
+
+        private double z = 0;
+        public double Z
+        {
+            get { return z; }
+            set
+            {
+                if (z != value)
+                {
+                    z = value;
+                    OnPropertyChanged("Z");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+
+        public GyroscopeTest()
+        {
+            // Register for reading changes.
+            Gyroscope.ReadingChanged += Gyroscope_ReadingChanged;
+        }
+
+        void Gyroscope_ReadingChanged(object sender, GyroscopeChangedEventArgs e)
+        {
+            var data = e.Reading;
+            // Process Angular Velocity X, Y, and Z reported in rad/s
+            X += (180 / Math.PI) * data.AngularVelocity.X / 15;
+            Y += (180 / Math.PI) * data.AngularVelocity.Y / 15;
+            Z += (180 / Math.PI) * data.AngularVelocity.Z / 15;
+            //Console.WriteLine(DateTime.Now); // 15 раз в секунду +-1 раз
+            Console.WriteLine($"Reading: X: {data.AngularVelocity.X}, Y: {data.AngularVelocity.Y}, Z: {data.AngularVelocity.Z}");
+        }
+
+        public void ToggleGyroscope()
+        {
+            try
+            {
+                if (Gyroscope.IsMonitoring)
+                    Gyroscope.Stop();
+                else
+                {
+                    Gyroscope.Start(speed);
+                    X = 0;
+                    Y = 0;
+                    Z = 0;
+                }
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Feature not supported on device
+            }
+            catch (Exception ex)
+            {
+                // Other error has occurred.
+            }
+        }
+    }
+}
---
+++ test3/test3/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace test3
+{
+    public partial class MainPage : ContentPage
+    {
+        //private GyroscopeTest gyro;
+        public MainPage()
+        {
+            InitializeComponent();
+            //gyro = new GyroscopeTest();
+            //gyro.ToggleGyroscope();
+            //Label x_value_label = new Label();
+            //Binding gyro_binding = new Binding { Source = gyro, Path = "x" };
+            //x_value_label.SetBinding(Label.TextProperty, gyro_binding);
+        }        
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "Старт")
+                button1.Text = "Стоп";
+            else
+                button1.Text = "Старт";
+            (this.Resources["gyro"] as GyroscopeTest).ToggleGyroscope();
+        }
+    }
+}
